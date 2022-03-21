901bb63a421195f61677455925c570d10600cb3a
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-03-21 11:36:01 -0500

    add new routes

--- app.js
+++ app.js
@@ -1,8 +1,56 @@
 const express = require('express')
+const cors = require('cors')
+const req = require('express/lib/request')
 
 const app = express()
 const PORT = process.env.PORT || 3001
 // Your Code Here
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get(
+  '/middleware',
+  (request, response, next) => {
+    console.log(request.method)
+    next()
+  },
+  (req, res) => {
+    res.send(`the HTTP type of ${req.method} request is complete`)
+  }
+)
+app.post(
+  '/middleware',
+  (request, response, next) => {
+    console.log(request.method)
+    next()
+  },
+  (req, res) => {
+    res.send(`the HTTP type of ${req.method} request is complete`)
+  }
+)
+app.delete(
+  '/middleware',
+  (request, response, next) => {
+    console.log(request.method)
+    next()
+  },
+  (req, res) => {
+    res.send(`the HTTP type of ${req.method} request is complete`)
+  }
+)
+app.put(
+  '/middleware',
+  (request, response, next) => {
+    console.log(request.method)
+    next()
+  },
+  (req, res) => {
+    res.send(`the HTTP type of ${req.method} request is complete`)
+  }
+)
+
 
 // Your Code Ends Here
 app.listen(PORT, () => {
